a5d850b418e89e8efbe90e5db67667027e9dbda1
Jugal Mahida <jugal.mahida.work@gmail.com>
2025-11-22 15:49:36 +0530

    Update GitHub OAuth callback URL to include '/login/github/callback' for production environment

--- src/config/oauth.config.js
+++ src/config/oauth.config.js
@@ -6,5 +6,5 @@ export const github = new GitHub(
   process.env.GITHUB_CLIENT_SECRETS,
   process.env.NODE_ENV === "development"
     ? `${process.env.LOCAL_FRONTEND_URL}/login/github/callback`
-    : process.env.FRONTEND_URL
+    : `${process.env.FRONTEND_URL}/login/github/callback`
 );
